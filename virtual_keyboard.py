d0f8d627c825fc2e1a946c01c846220ff35df555
doquan01 <55120808+doquan01@users.noreply.github.com>
2022-04-09 18:39:36 -0700

    Update virtual_keyboard.py

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -1,3 +1,4 @@
+from ast import Delete
 import cv2
 import cvzone
 import mediapipe
@@ -11,28 +12,31 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
-keyboard_keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-                  ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-                  ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
-                  ["1","2","3","4","5","6","7","8","9"],
-                  ["0","SPA","DEL","?","!","'","+","-","*"]
-                  ]
+# keyboard_keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+#                   ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+#                   ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
+#                   ["1","2","3","4","5","6","7","8","9"],
+#                   ["0","?","!","'","+","-","*"]
+#                   ]
+
+keyboard_keys = [["o"]]
 
 final_text = ""
 
 keyboard = Controller()
 
 
-def draw(img, buttonList):
-    for button in buttonList:
-        x, y = button.pos
-        w, h = button.size
-        cvzone.cornerRect(img, (button.pos[0], button.pos[1],
-                                                   button.size[0],button.size[0]), 20 ,rt=0)
-        cv2.rectangle(img, button.pos, (int(x + w), int(y + h)), (255, 144, 30), cv2.FILLED)
-        cv2.putText(img, button.text, (x + 20, y + 65),
-                    cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
-    return img
+# def draw(img, buttonList):
+#     for button in buttonList:
+#         x, y = button.pos
+#         w, h = button.size
+        
+#         cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0],button.size[0]), 20 ,rt=0)
+#         cv2.rectangle(img, button.pos, (int(x + w), int(y + h)), (50, 50, 50), cv2.FILLED) #text box color #cv2.filled is our webcam playback
+
+#         cv2.putText(img, button.text, (x + 20, y + 65),
+#                     cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 4)
+#     return img
 
 
 def transparent_layout(img, buttonList):
@@ -40,8 +44,9 @@ def transparent_layout(img, buttonList):
     for button in buttonList:
         x, y = button.pos
         cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1],button.size[0],button.size[0]), 20 ,rt=0)
-        cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),(255, 144, 30), cv2.FILLED)
+        cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]), (50, 50, 50), cv2.FILLED)
         cv2.putText(imgNew, button.text, (x + 20, y + 65),cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+        #cv2.putText(imgNew, button.text, text_offset_x=20, text_offset_y=20, vspace=10, hspace=10, font_scale=1.0, background_RGB=(228,255,222),text_RGB=(255,255,1),thickness=1, alpha=0.5)
 
     out = img.copy()
     alpaha = 0.5
@@ -52,10 +57,18 @@ def transparent_layout(img, buttonList):
 
 
 class Button():
-    def __init__(self, pos, text, size=[85, 85]):
+    def __init__(self, pos, text="", size=[85, 85]):
         self.pos = pos
         self.size = size
         self.text = text
+
+class DeleteButton(Button):
+    def __init__(self, pos, previous, size=[85,85] ):
+        self.pos = pos
+        self.size = size
+
+        
+
 
 
 buttonList = []
@@ -70,7 +83,7 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bboxInfo = detector.findPosition(img)
-    img = draw(img, buttonList)  # change the draw funtion to transparent_layout for transparent keys
+    img = transparent_layout(img, buttonList)  # change the draw funtion to transparent_layout for transparent keys
 
     if lmList:
         for button in buttonList:
