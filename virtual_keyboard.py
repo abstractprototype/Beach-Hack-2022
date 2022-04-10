1d7c12ec3c0643e8c57583f794cd2f64499d2457
doquan01 <55120808+doquan01@users.noreply.github.com>
2022-04-09 23:57:10 -0700

    Added Score

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -5,38 +5,21 @@ import mediapipe
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
 import numpy as np
-from pynput.keyboard import Controller
+# from pynput.keyboard import Controller
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 1280)
 cap.set(4, 720)
+ 
 
 detector = HandDetector(detectionCon=0.8)
-# keyboard_keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-#                   ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-#                   ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
-#                   ["1","2","3","4","5","6","7","8","9"],
-#                   ["0","?","!","'","+","-","*"]
-#                   ]
+
+score = 0
+counter = 0
 
 keyboard_keys = [["o"]]
 
-final_text = ""
-
-keyboard = Controller()
-
-
-# def draw(img, buttonList):
-#     for button in buttonList:
-#         x, y = button.pos
-#         w, h = button.size
-        
-#         cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0],button.size[0]), 20 ,rt=0)
-#         cv2.rectangle(img, button.pos, (int(x + w), int(y + h)), (50, 50, 50), cv2.FILLED) #text box color #cv2.filled is our webcam playback
-
-#         cv2.putText(img, button.text, (x + 20, y + 65),
-#                     cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 4)
-#     return img
+# keyboard = Controller()
 
 
 def transparent_layout(img, buttonList):
@@ -46,8 +29,7 @@ def transparent_layout(img, buttonList):
         cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1],button.size[0],button.size[0]), 20 ,rt=0)
         cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]), (50, 50, 50), cv2.FILLED)
         cv2.putText(imgNew, button.text, (x + 20, y + 65),cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
-        #cv2.putText(imgNew, button.text, text_offset_x=20, text_offset_y=20, vspace=10, hspace=10, font_scale=1.0, background_RGB=(228,255,222),text_RGB=(255,255,1),thickness=1, alpha=0.5)
-
+    
     out = img.copy()
     alpaha = 0.5
     mask = imgNew.astype(bool)
@@ -61,51 +43,52 @@ class Button():
         self.pos = pos
         self.size = size
         self.text = text
-
-class DeleteButton(Button):
-    def __init__(self, pos, previous, size=[85,85] ):
-        self.pos = pos
-        self.size = size
-
         
 
-
-
 buttonList = []
-# mybutton = Button([100, 100], "Q")
 for k in range(len(keyboard_keys)):
     for x, key in enumerate(keyboard_keys[k]):
         buttonList.append(Button([100 * x + 25, 100 * k + 50], key))
-        #print(x, key)
 
+inProgress = False
+isOver = False
 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bboxInfo = detector.findPosition(img)
-    img = transparent_layout(img, buttonList)  # change the draw funtion to transparent_layout for transparent keys
+    img = transparent_layout(img, buttonList)
 
-    if lmList:
-        for button in buttonList:
-            x, y = button.pos
-            w, h = button.size
+    if inProgress == True:
+        if(isOver == False):
+            cv2.putText(img,'SCORE: ' + str(score),(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2,cv2.LINE_AA)
+        elif(isOver == True):
+            cv2.putText(img,'<- GAME OVER ->',(320,350),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),5,cv2.LINE_AA)
+        
+        # change the draw funtion to transparent_layout for transparent keys
 
-            if x < lmList[8][0]<x+w and y < lmList[8][1] < y+h:
-                cv2.rectangle(img, button.pos, (x + w, y + h),
-                              (0, 255, 255), cv2.FILLED)
-                cv2.putText(img, button.text, (x + 20, y + 65),
-                            cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
-                l, _, _ = detector.findDistance(8,12, img, draw=False)
-                print(l)
+        if lmList:
+            for button in buttonList:
+                x, y = button.pos
+                w, h = button.size
 
-                if l < 25:
-                    keyboard.press(button.text)
+                if x < lmList[8][0]<x+w and y < lmList[8][1] < y+h:
                     cv2.rectangle(img, button.pos, (x + w, y + h),
-                                  (0, 255, 0), cv2.FILLED)
+                                (0, 255, 255), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65),
                                 cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
-                    final_text += button.text
-                    sleep(0.20)
+                    l, _, _ = detector.findDistance(8,12, img, draw=False)
+                    print(l)
+
+                    if l < 25:
+                        # keyboard.press(button.text)
+                        score += 1
+                        cv2.rectangle(img, button.pos, (x + w, y + h),
+                                    (0, 255, 0), cv2.FILLED)
+                        cv2.putText(img, button.text, (x + 20, y + 65),
+                                    cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+                        # final_text += button.text
+                        sleep(0.20)
 
     # cv2.rectangle(img, (25,350), (700, 450),
     #               (255, 255, 255), cv2.FILLED)
@@ -118,5 +101,12 @@ while True:
     #             (0, 0, 0), 5)
 
     # img = mybutton.draw(img)
+    elif(inProgress == False):
+        time = 500
+        cv2.putText(img,'GAME WILL START IN --> ' + str(time - counter),(200,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3,cv2.LINE_AA)
+        counter = counter + 1
+        if counter > time:
+            inProgress = True
+        
     cv2.imshow("output", img)
     cv2.waitKey(1)
